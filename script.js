524f4be01d78d5f9aa2e26a24e1a08762d97fe66
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2024-08-05 19:28:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const excerpt = document.getElementById('excerpt')
 const profile_img = document.getElementById('profile_img')
 const name = document.getElementById('name')
 const date = document.getElementById('date')
-
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
@@ -20,7 +19,6 @@ function getData() {
     '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
   name.innerHTML = 'John Doe'
   date.innerHTML = 'Oct 08, 2020'
-
   animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
   animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
 }
